db9c9f85d075ddbbda7143a1315ef96e770521d5
Kris Pakala <kris@cygnuss.com>
2022-12-15 22:00:40 -0500

    Added TwitterStats.Models

---
+++ TwitterStats.Models/Tweet.cs
@@ -0,0 +1,14 @@
+﻿namespace TwitterStats.Models
+{
+    public class Tweet
+    {
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public List<string> edit_history_tweet_ids { get; set; }
+        public string id { get; set; }
+        public string text { get; set; }
+    }
+}
